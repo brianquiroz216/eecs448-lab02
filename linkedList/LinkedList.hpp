89a43a789f77e566c17d6c794155c2a209409f83
Brian Quiroz <r229q057@ku.edu>
2018-01-29 16:17:33 -0600

    Fixed search method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,11 +37,11 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-
-	/** TODO
-		Fix this method
-	*/
-
+	while (temp->getNext() != nullptr) {
+		if (temp->getValue() == value) {
+			isFound = true;
+		}
+	}
 	return(isFound);
 }
 
